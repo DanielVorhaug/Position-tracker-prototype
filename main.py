aee9df880fb6dc7e20ce128c3576956d9dd79395
Daniel <dvorhaug@gmail.com>
2021-02-23 15:54:49 +0100

    Tracking position over large range

--- main.py
+++ main.py
@@ -17,19 +17,28 @@ def main():
     #while keyboard.is_pressed("q") q== False:
 
     movement = [0,0]
-    angle = 0#math.pi/4
+    angle = 0 #math.pi/4
     sensitivity = 1
+    filter_factor = 0.2
+
+    coordinate_zero = [int(1920/2) ,int(1080/2)]
+
+    position = [0,0]
+
     while keyboard.is_pressed("q") == False: 
         
         coordinates = pyautogui.position()
+
+        win32api.SetCursorPos((coordinate_zero[0], coordinate_zero[1]))
 
         data = arduino.readline()
         data = data.decode("utf-8")
 
         if len(data) > 0:
             try:
-                angle = -float(data.strip().strip(".")) * math.pi / 180 + 0.2
-                print(angle)
+                angle_new = -float(data.strip().strip(".")) * math.pi / 180 + 0.2
+                angle = (1 - filter_factor) * angle + filter_factor * angle_new
+                #print(angle)
             except:
                 print("crash")
                 angle = 0
@@ -39,13 +48,21 @@ def main():
         #time.sleep(0.001)
         new_coordinates = pyautogui.position()
 
-        dx = new_coordinates[0] - coordinates[0]
-        dy = new_coordinates[1] - coordinates[1]
+        dx = coordinates[0] - coordinate_zero[0]
+        dy = coordinates[1] - coordinate_zero[1]
 
-        movement[0] = int((dx * math.cos(angle) + dy * math.sin(angle)) / sensitivity)
-        movement[1] = int((dy * math.cos(angle) - dx * math.sin(angle)) / sensitivity)
+        movement[0] = (dx * math.cos(angle) + dy * math.sin(angle)) / sensitivity
+        movement[1] = (dy * math.cos(angle) - dx * math.sin(angle)) / sensitivity
 
-        win32api.SetCursorPos((coordinates[0] + movement[0], coordinates[1] + movement[1]))
+        position[0] += movement[0]
+        position[1] += movement[1]
+
+        print(position, "\t", angle)
+
+        
+
+        if keyboard.is_pressed("n") == True:
+            position = [0,0]
 
         # new_coordinate = list(pyautogui.position())
         # dCoordinate = [new_coordinate[0] - coordinate[0], new_coordinate[1] - coordinate[1]]
